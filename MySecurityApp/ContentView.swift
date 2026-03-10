88716aed9584a0277afeae4fcc460e05a01678fb
thanhtan541 <thanhtan004100@gmail.com>
2026-03-10 17:31:54 +0700

    Initial Commit

---
+++ MySecurityApp/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  MySecurityApp
+//
+//  Created by nguyen thanh tan on 10/3/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ MySecurityApp/MySecurityAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  MySecurityAppApp.swift
+//  MySecurityApp
+//
+//  Created by nguyen thanh tan on 10/3/26.
+//
+
+import SwiftUI
+
+@main
+struct MySecurityAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
