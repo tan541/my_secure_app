70d0586cf72f3a55fe0f03c801b1c21640bf4814
thanhtan541 <thanhtan004100@gmail.com>
2026-03-26 15:15:39 +0700

    feat: update code

--- MySecurityApp/AppDelegate.swift
+++ MySecurityApp/AppDelegate.swift
@@ -2,10 +2,35 @@ import Cocoa
 import SystemExtensions
 
 class AppDelegate: NSObject, NSApplicationDelegate, OSSystemExtensionRequestDelegate {
+    var extensionBundleID = "com.uney.shieldnetdefense.Extension"
 
     func applicationDidFinishLaunching(_ notification: Notification) {
+        parseCommandLineArguments()
         NSLog("Native Helper: App launched. Starting activation...")
         activateExtension()
+    }
+    
+    private func parseCommandLineArguments() {
+        let arguments = ProcessInfo.processInfo.arguments
+        
+        for i in 0..<arguments.count {
+            if arguments[i] == "--extension-id", i+1 < arguments.count {
+                extensionBundleID = arguments[i+1]
+            }
+            
+            if arguments[i] == "--user-id", i+1 < arguments.count {
+                NSLog("Native Helper: Received user-id = %@", arguments[i+1])
+                // Store it, write to file, or use it later
+            }
+            
+            if arguments[i] == "--config", i+1 < arguments.count {
+                if let data = arguments[i+1].data(using: .utf8),
+                   let json = try? JSONSerialization.jsonObject(with: data) as? [String: Any] {
+                    NSLog("Native Helper: Received config: %@", json)
+                    // You can store this dictionary for later use
+                }
+            }
+        }
     }
 
     func activateExtension() {
